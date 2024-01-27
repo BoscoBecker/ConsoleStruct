f3074dc31848a6cb537da91d61c520d6f2f3eb0a
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-01-27 20:31:57 -0300

    Adicionar arquivos de projeto.

---
+++ Point.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleStruct
+{
+    struct Point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString() {
+            return "(" + X + ", " + Y + ")";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ConsoleStruct;
+namespace ConsoleStruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point P = new Point(); ///or  P.X = 10.0;, resumindo inicializar o atributo funciona
+            P.X = 10.0;
+            Console.WriteLine(P);
+        }
+    }
+}
